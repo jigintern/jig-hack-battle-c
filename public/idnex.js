cd6246ecac902696491dae5cc5d3cf60e7c3212f
curry-pan <hibiki.19n1102209@gmail.com>
2023-12-28 15:06:51 +0900

    d

--- public/idnex.js
+++ public/idnex.js
@@ -164,9 +164,9 @@ const loadFaceParts = () => {
         if (child.isMesh) {
           child.position.x = 0;
           child.position.y = 0;
-          child.position.z = -3;
-          child.rotation.x = 0;
-          child.rotation.z = 0;
+          child.position.z = -1;
+          child.rotation.x = Math.PI / 2;
+          child.rotation.z = Math.PI / 2;
           child.scale.x = 3;
           child.scale.y = 3;
           child.scale.z = 3;
