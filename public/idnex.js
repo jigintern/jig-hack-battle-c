1072273bc4ad1e37e55d4ca7034cd5a989ca6662
curry-pan <hibiki.19n1102209@gmail.com>
2023-12-28 15:48:37 +0900

    a

--- public/idnex.js
+++ public/idnex.js
@@ -166,7 +166,7 @@ const loadFaceParts = () => {
       object.traverse(function (child) {
         if (child.isMesh) {
           child.position.x = 0;
-          child.position.y = 0;
+          child.position.y = -1;
           child.position.z = -1;
           child.rotation.x = Math.PI / 2;
           child.rotation.z = Math.PI / 2;
